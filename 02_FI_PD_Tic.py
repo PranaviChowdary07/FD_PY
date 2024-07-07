f407ae93d5a1e4201e6985459816bd858faaf8ab
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-07-07 11:24:31 +0530

    Crete loop

--- 02_FI_PD_Tic.py
+++ 02_FI_PD_Tic.py
@@ -1,1 +1,21 @@
-import tinker 
+import tkinter
+
+#Players
+playerX = "X"
+playerO = "O"
+curr_player = playerX
+
+board = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
+color_blue = "#4584b6"
+color_yellow = "#ffde57"
+color_gray = "#343434"
+color_light_gray = "#646464"
+
+# Window 
+window = tkinter.Tk()  # Create game wimdow
+window.title("Tic Tac Toe")
+window.resizable(False, False)
+# create loop for window open
+window.mainloop()
