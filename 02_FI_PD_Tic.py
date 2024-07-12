7a160db727198763cde6cbc5489b7586047595d4
PranaviChowdary07 <pranavikasthuri07@gmail.com>
2024-07-12 10:57:52 +0530

    Add origin

--- 02_FI_PD_Tic.py
+++ 02_FI_PD_Tic.py
@@ -2,6 +2,8 @@ import tkinter   # tk - interface(graphical) user interface library
 
 def set_title(row,column):
     pass
+def new_game():
+    pass
 #Players
 playerX = "X"
 playerO = "O"
@@ -23,12 +25,26 @@ window.resizable(False, False)
 # components for game
 frame = tkinter.Frame(window)
 label = tkinter.Label(frame,text=  curr_player+"'S turn",font  = ("Consolas", 20),background= color_gray,foreground="white")
-label.grid(row =0,column=0,columnspan=3)
+label.grid(row =0,column=0,columnspan=3, sticky="we")
 
 for row in range(3):
     for column in range(3):
-        board[row][column] = tkinter.Button(frame, text ="",font=("Consolas", 50,"bold"),background= color_gray,foreground=color_blue,width=4,height=1,command=lambda row = row ,column= column:set_title(row,column))
+        board[row][column] = tkinter.Button(frame, text ="",font=("Consolas", 50,"bold"),background= color_gray,    foreground=color_blue,width=4,height=1,command=lambda row = row ,column= column:set_title(row,column))
         board[row][column].grid(row = row+1,column= column)
+button = tkinter.Button(frame, text = "restart", font = ("Consolas",20),background=color_gray,foreground="white",   command=new_game)
+button.grid(row=4,column=0,columnspan=3,sticky="we")
+
 frame.pack()
+# Center for window
+window.update()
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+# format "(w)x(h)+(x)+(y)"
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 # create loop for window open
 window.mainloop()
